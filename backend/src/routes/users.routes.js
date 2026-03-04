a710473c2f12b96968795328a676be305e257fed
david-espindola01 <david.espindola01@uptc.edu.co>
2026-03-04 15:15:23 -0500

    feature: new routing for users

---
+++ backend/src/routes/users.routes.js
@@ -0,0 +1,9 @@
+const { Router } = require('express')
+const { getUsers } = require('../controllers/users.controller')
+const { authenticateAdmin } = require('../middleware/auth.middleware')
+
+const router = Router()
+
+router.get('/', authenticateAdmin, getUsers)
+
+module.exports = router
