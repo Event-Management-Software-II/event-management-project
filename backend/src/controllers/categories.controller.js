4134eba8d9216a48df16537aa53bb0a90620a33a
david-espindola01 <david.espindola01@uptc.edu.co>
2026-04-05 21:50:48 -0500

    fix: prisma issues repaired by connecting successfully by the prisma config

--- backend/src/controllers/categories.controller.js
+++ backend/src/controllers/categories.controller.js
@@ -1,6 +1,5 @@
-const { PrismaClient, Prisma } = require('@prisma/client');
-
-const prisma = new PrismaClient();
+const { Prisma } = require('@prisma/client');
+const prisma = require('../prisma/prisma');
 
 const getCategories = async (req, res) => {
   try {
