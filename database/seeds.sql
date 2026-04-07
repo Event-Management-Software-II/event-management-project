68862e57791d9902eb58ef20e2bdfe0c995fca40
david-espindola01 <david.espindola01@uptc.edu.co>
2026-04-07 14:30:15 -0500

    fix: database names changed into some new names

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,12 +1,12 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-INSERT INTO "Role" ("name") VALUES ('admin') ON CONFLICT DO NOTHING;
-INSERT INTO "Role" ("name") VALUES ('user')  ON CONFLICT DO NOTHING;
+INSERT INTO "Role" ("roleName") VALUES ('admin') ON CONFLICT DO NOTHING;
+INSERT INTO "Role" ("roleName") VALUES ('user')  ON CONFLICT DO NOTHING;
 
 INSERT INTO "User" ("email", "password", "full_name", "id_role")
 VALUES (
     'admin@eventos.com',
     crypt('admin123', gen_salt('bf', 10)),
     'Administrador',
-    (SELECT "id_role" FROM "Role" WHERE "name" = 'admin')
+    (SELECT "id_role" FROM "Role" WHERE "roleName" = 'admin')
 ) ON CONFLICT DO NOTHING;
